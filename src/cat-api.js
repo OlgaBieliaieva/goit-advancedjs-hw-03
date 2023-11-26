6d0b3ab194c54fa3eb32660d33658158ef559193
OlgaBieliaieva <belyaevaolya0@gmail.com>
2023-11-26 17:10:00 +0200

    finished cat-search

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const URL_FOR_ALL_BREEDS = 'https://api.thecatapi.com/v1/breeds/?';
-const URL_FOR_SELECTED_BREED = ' https://api.thecatapi.com/v1/images/search';
+const URL_FOR_SELECTED_BREED = ' https://api.thecatapi.com/v1/images/search?';
 const REQUEST_OPTIONS = {
   api_key:
     'live_4OV6mO4kCm8Wvp0obaFUUKjV7Fg0aDGCrA5iSKIh93pBZ22atpg8saVdJ0BQ988s',
-  breed_ids: '',
+  breed_ids: '',  
 };
 
 export async function fetchBreeds() {
@@ -14,8 +14,7 @@ export async function fetchBreeds() {
     const response = await axios.get(`${URL_FOR_ALL_BREEDS}${options}`);
     return response.data;
   } catch (error) {
-    console.log(error);
-    // throw new Error
+    console.log(error);    
   }
 }
 
@@ -23,9 +22,9 @@ export async function fetchCatByBreed(breedId) {
   if (REQUEST_OPTIONS.breed_ids !== breedId) {
     REQUEST_OPTIONS.breed_ids = breedId;
   }
-  const options = new URLSearchParams(REQUEST_OPTIONS);
+  const options = new URLSearchParams({...REQUEST_OPTIONS, limit: 15});
   try {
-    const response = await axios.get(`${URL_FOR_SELECTED_BREED}${options}`);
+    const response = await axios.get(`${URL_FOR_SELECTED_BREED}${options}`);    
     return response.data;
   } catch (error) {
     console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,318 @@
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
-import Notiflix from 'notiflix';
+import iziToast from 'izitoast';
+import Swiper from 'swiper/bundle';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const refs = {
-  searchSelect: document.querySelector('.breed-select'),  
+  searchSelect: document.querySelector('.breed-select'),
   searchResult: document.querySelector('.cat-info'),
+  swiperContainer: document.querySelector('.swiper-container'),
+  swiper: document.querySelector('.swiper'),
+  swiperWrapper: document.querySelector('.swiper-wrapper'),
+  descriptionWrapper: document.querySelector('.description-container'),
+  loader: document.querySelector('.loader'),
 };
-  refs.searchSelect.addEventListener('change', getSelectedBreed)
 
-  fetchBreeds()
+refs.searchSelect.addEventListener('change', getSelectedBreed);
+
+fetchBreeds()
   .then(data => createSelectOptions(data))
   .catch(() => showError());
-  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
 
 function createSelectOptions(data) {
-  console.log(data);
   const optionsMarkup = data
-    .map(({ id, name }) => `<option class="breed-option" value="${id}">${name}</option>`)
+    .map(
+      ({ id, name }) =>
+        `<option class="breed-option" value="${id}">${name}</option>`
+    )
     .join('');
-    refs.searchSelect.insertAdjacentHTML('beforeend', optionsMarkup)
+  refs.searchSelect.insertAdjacentHTML('beforeend', optionsMarkup);
 }
 
 function getSelectedBreed(event) {
-  console.log(event);
+  refs.loader.classList.remove('visually-hidden');
   const breedId = event.target.value;
-  console.log(breedId); 
-  fetchCatByBreed(breedId).then().catch(Notify.failure('Oops! Something went wrong! Try reloading the page!'));
+  if (breedId !== '') {
+    refs.swiperContainer.classList.add('visually-hidden');
+    refs.descriptionWrapper.innerHTML = '';
+    refs.swiperWrapper.innerHTML = '';
+    fetchCatByBreed(breedId)
+      .then(data => createCatInfoMarkup(data))
+      .catch(e => showError(e));
+  } else {
+    refs.descriptionWrapper.innerHTML = '';
+    refs.swiperContainer.innerHTML = '';
+    refs.loader.classList.add('visually-hidden');
+  }
 }
 
-function showError() {
-  // console.log(error);
- return Notify.failure('Oops! Something went wrong! Try reloading the page!')
+function createCatInfoMarkup(data) {
+  const catInfo = data[0].breeds[0];
+  const imageList = data.map(item => item.url);
+  const ratingStars = [1, 2, 3, 4, 5];
+
+  const swiperMarkup = imageList
+    .map(
+      url =>
+        `        
+        <div class="swiper-slide"><img class="slide-img" src="${url}" alt="${catInfo.name}" width="500" height="376"></div>`
+    )
+    .join('');
+
+  const catInfoMarkup = `<h2>${catInfo.name}</h2>
+    <p class="label">Origin: <span class="text">${catInfo.origin}</span></p>    
+    <p class="label">Weight: <span class="text">${
+      catInfo.weight.metric
+    }</span> kg</p>
+    <p class="label">Weight: <span class="text">${
+      catInfo.weight.metric
+    }</span> kg</p>
+    <p class="label">Lifetime: <span class="text">${
+      catInfo.life_span
+    }</span> yrs</p>
+    <p class="label">Weight: <span class="text">${
+      catInfo.weight.metric
+    }</span> kg</p>
+    <p class="label">Temperament: <span class="text">${
+      catInfo.temperament
+    }</span></p>
+    <p class="description">${catInfo.description}</p>
+    <ul class="properties">
+    <li>
+    <p class="label">Affection level</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.affection_level >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Adaptability</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.adaptability >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Child friendly</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.child_friendly >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Dog friendly</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.dog_friendly >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Energy level</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.energy_level >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Grooming</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.grooming >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Health issues</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.health_issues >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Intelligence</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.intelligence >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Shedding level</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.shedding_level >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Social needs</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.social_needs >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Stranger friendly</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.stranger_friendly >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    <li>
+    <p class="label">Vocalisation</p>
+    <ul class="stars">
+    ${ratingStars
+      .map(item =>
+        catInfo.vocalisation >= item
+          ? `<li><svg width="30" height="30">
+    <use href="/images/star-rating.icons.svg#star-filled"></use>
+  </svg></li>`
+          : `<li><svg width="30" height="30">
+        <use href="/images/star-rating.icons.svg#star-empty"></use>
+      </svg></li>`
+      )
+      .join('')}
+    </ul>    
+    </li>
+    </ul>
+    <a href="${catInfo.wikipedia_url}"> Learn more </a>`;
+
+  refs.loader.classList.add('visually-hidden');
+  refs.swiperWrapper.insertAdjacentHTML('afterbegin', swiperMarkup);
+  initSwiper();
+  refs.swiperContainer.classList.remove('visually-hidden');
+  refs.descriptionWrapper.insertAdjacentHTML('beforeend', catInfoMarkup);
+}
+
+function initSwiper() {
+  const swiperOptions = {
+    modules: [Pagination, Navigation],
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    speed: 1000,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  };
+  const swiper = new Swiper('.swiper', { ...swiperOptions });
+  refs.swiper.addEventListener('mouseover', function () {
+    swiper.autoplay.stop();
+  });
+
+  refs.swiper.addEventListener('mouseout', function () {
+    swiper.autoplay.start();
+  });
+}
+
+function showError(e) {
+  iziToast.error({
+    title: 'Oops!',
+    message: 'Something went wrong! Try reloading the page!',
+  });
 }
